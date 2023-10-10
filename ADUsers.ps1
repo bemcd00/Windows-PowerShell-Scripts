830cb12eb29bd2cfd6426919b761e8239f507871
Briannah McDaniel <109481233+bemcd00@users.noreply.github.com>
2023-10-10 11:18:46 -0500

    Update ADUsers.ps1

--- ADUsers.ps1
+++ ADUsers.ps1
@@ -9,7 +9,7 @@ Created By: Briannah McDaniel
 
 Write-Output "`n------------------------------"
 Write-Output "List of Domain Admins:"     #prints "List of Domain Admins: "
-Get-ADGroupMember 'domain admins' | Where-Object -Property name -ne 'krbtgt' | select name     #Gets the names of Users that are Domain Admins
+Get-ADGroupMember 'domain admins' | Where-Object -Property name -ne 'krbtgt' | select name, SamAccountName     #Gets the names and username of Users that are Domain Admins
 #$NumDA is a variable used to store the number of Domain Admins      
 $NumDA=(Get-ADGroupMember 'domain admins' | Where-Object -Property name -ne 'krbtgt' | select name).count     #Counts how many users are Domain Admins
 write-output "`nNumber of Domain Admins: $NumDA"     #Prints out the number of Domain Admins
@@ -19,8 +19,11 @@ Write-Output "`n------------------------------"
 Write-Output "List of Domain Users:`n"     #prints "List of Domain Users: "
 Write-Output "name"     #prints "name"
 Write-Output "----"     #prints "----"
-Get-ADGroupMember 'domain users' | Where-Object -Property name -ne 'krbtgt' | select name     #Gets the names of Users that are Domain Users
+Get-ADGroupMember 'domain users' | Where-Object -Property name -ne 'krbtgt' | select name, SamAccountName     #Gets the names and usernames of Users that are Domain Users
 #NumDU is a variable used to store the number of Domain Users
 $NumDU=(Get-ADGroupMember 'domain users' | Where-Object -Property name -ne 'krbtgt' | select name).count     #Counts how many users are Domain Users
-write-output "`nNumber of Domain Users: $NumDU"     #Prints out the number of Domain Users
-Write-Output "------------------------------"
+write-output "`nNumber of Domain Users: $NumDU"     #Prints out the number of Domain dUmins
+Write-Output "------------------------------`n"
+
+# Gets the ratio of Domain Admins and Domain Users.
+Write-Output "Domain Admins: ${NumDA}:$NumDU Domain Users"
